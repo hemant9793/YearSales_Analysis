49eab83440744c92dd3bd17d12aa9e2759c766ef
hemant9793 <hemant.9793@gmail.com>
2021-02-16 07:50:36 +0530

    initial commit

--- Best_sales-month.py
+++ Best_sales-month.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt
 
 #reading single file of all data
 sales_data = pd.read_csv("E:\deepak singh\All_months_data.csv",parse_dates=["Order Date"])
@@ -20,6 +20,31 @@ sales_data = sales_data[sales_data["Order Date"].str[0:2] != 'Or']
 #adding additional columns for helping with data analysis and changing its type to int
 sales_data["Months"] = sales_data["Order Date"].str[0:2]
 sales_data["Months"] = sales_data["Months"].astype('int')
-print(sales_data.head())
+
 
 #best month for sale and how much was earned in that month
+
+#adding sales column to ans our question
+sales_data['Quantity Ordered'] = pd.to_numeric(sales_data['Quantity Ordered'])
+sales_data['Price Each'] = pd.to_numeric(sales_data['Price Each'])
+sales_data["Sales"] = sales_data['Quantity Ordered'] * sales_data['Price Each']
+
+
+#getting the total of each month
+monthly_sale = sales_data.groupby(by='Months')
+
+list=[]
+month =[]
+for key,df in monthly_sale:
+    month.append(key)
+    list.append(df["Sales"].sum())
+a=list.index(max(list))
+
+#plotting bar chart
+
+plt.bar(month,list)
+plt.xlabel("Months")
+plt.ylabel("Sales")
+plt.xticks(month)
+plt.show()
+#as our bar chart shows that best month for sales was December.
